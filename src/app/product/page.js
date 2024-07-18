dc2a5dc76db343286f6ca759895bd75fadff81ae
chinmaya-elred <chinmaya@elred.io>
2024-07-18 19:33:25 +0530

    completed till section 26

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,9 +1,43 @@
-import React from 'react'
+'use client';
+import React from "react";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const NavLinks = [
+	{ id: 1, name: 'Home', href: '/' },
+	{ id: 2, name: 'Blog', href: '/blog' },
+	{ id: 3, name: 'About', href: '/about' },
+];
 
 function Product() {
+  const pathname = usePathname()
+
   return (
-    <div>Product Page</div>
-  )
+    <div>
+      Product Page
+      <h1>
+        <b>Link Component</b>
+      </h1>
+      <nav>
+        <ul>
+          {NavLinks.map((link) => {
+            	let isActive =pathname.startsWith(link.href)
+
+            return (
+              <li key={link.id}>
+                <Link
+                  href={link.href}
+                  className={isActive ? "font-bold" : "bg-blue-400"}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,14 +1,22 @@
-import React from 'react'
+"use client"; 
+import React from "react";
+import { useRouter } from 'next/navigation'
 
-export const metadata = {
-  title: "This is profile",
-  description: "Profile description is added",
-};
+// export const metadata = {
+//   title: "This is profile",
+//   description: "Profile description is added",
+// };
 
 function index() {
+  const router = useRouter()
   return (
-    <div style={{ backgroundColor: 'red'}}>profile</div>
-  )
+    <div style={{ backgroundColor: "red" }}>
+      profile
+      <button onClick={() => router.push("/about")}>
+        Click here to read more
+      </button>
+    </div>
+  );
 }
 
-export default index
+export default index;
